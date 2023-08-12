57554d616c85a01b8222707e4408c0b561ab5d12
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-08-11 22:07:28 -0700

    updated console to handle all classes

--- console.py
+++ console.py
@@ -7,8 +7,16 @@ import shlex
 from datetime import datetime
 import models
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
-class_p = {"BaseModel": BaseModel}
+class_p = {"BaseModel": BaseModel, "Place": Place,
+           "Review": Review, "State": State, "User": User,
+           "Amenity": Amenity, "City": City}
 
 
 class HBNBCommand(cmd.Cmd):
